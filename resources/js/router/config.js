be7d845fdcc6def067262840d63b25929f50ca41
root <root@nms6.vir999.com>
2021-02-01 16:39:47 +0800

    mod router.js writing style

--- resources/js/router/config.js
+++ resources/js/router/config.js
@@ -1,21 +1,26 @@
-const routes = [
-    { path: "/", redirect: "/component1" },
-    {
-        path: "/component1",
-        component: () =>
-            import(
-                /* webpackChunkName: 'Component1' */ "../components/Component1"
-            ),
-        name: "Component1"
-    },
-    {
-        path: "/component2",
-        component: () =>
-            import(
-                /* webpackChunkName: 'Component2' */ "../components/Component2"
-            ),
-        name: "Component2"
-    }
-];
+// const routes = [
+//     { path: "/", redirect: "/component1" },
+//     {
+//         path: "/component1",
+//         component: () =>
+//             import(
+//                 /* webpackChunkName: 'Component1' */ "../components/Component1"
+//             ),
+//         name: "Component1"
+//     },
+//     {
+//         path: "/component2",
+//         component: () =>
+//             import(
+//                 /* webpackChunkName: 'Component2' */ "../components/Component2"
+//             ),
+//         name: "Component2"
+//     }
+// ];
 
-export default routes;
+export default {
+    Component1: () =>
+        import(/* webpackChunkName: 'Component1' */ "../components/Component1"),
+    Component2: () =>
+        import(/* webpackChunkName: 'Component2' */ "../components/Component2")
+};
