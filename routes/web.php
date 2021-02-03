d20c2eab06699ea0ce3982f327c4b9e2dbeaaf1a
root <root@nms6.vir999.com>
2021-02-03 09:04:41 +0800

    add routes on web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/{any}', function () {
+//     return view('welcome');
+// })->where('any', '.*');
+
+
 Route::get('/ex1', function () {
     return view('ex1');
 });
