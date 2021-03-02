807b2adb29ad6bebff0ac217de44b2c980a916d4
root <root@nms6.vir999.com>
2021-03-02 11:48:38 +0800

    test tailwindcss

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -3,11 +3,10 @@
         <template>
             <div>
                 this is App BasicLayout (App.vue)<br />
-                <router-link to="/">Component1</router-link> |
-                <router-link to="/component2">Component2</router-link> |
-                <router-link to="/dashboard">Dashboard</router-link> |
-                <router-link to="/events">Events</router-link> |
+                <router-link to="/">Component1</router-link> | <router-link to="/component2">Component2</router-link> |
+                <router-link to="/dashboard">Dashboard</router-link> | <router-link to="/events">Events</router-link> |
                 <router-link to="/counter">Counter</router-link> |
+                <router-link to="/abouttailwind">Tailwind</router-link> |
                 <hr />
                 <router-view />
             </div>
@@ -19,6 +18,6 @@
 export default {
     created() {
         console.log(process.env.MIX_DOG);
-    }
+    },
 };
 </script>
