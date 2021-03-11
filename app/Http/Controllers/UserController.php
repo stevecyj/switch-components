a85331947e336727d33564a78147db3673d80c81
root <root@nms6.vir999.com>
2021-03-11 14:42:24 +0800

    kinds of response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Services\ShortUrlService;
 use App\Http\Services\OpenDataUbikeService;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
@@ -17,6 +18,7 @@ class UserController extends Controller
         return response()->json(['user' => $user]);
     }
 
+    // request
     public function chkrequest(Request $request, $user)
     {
         $path = $request->path();
@@ -24,10 +26,23 @@ class UserController extends Controller
         $input = $request->all();
         $request_arr = ['request'=> $request,'path'=>$path,'url'=>$url,'user'=>$user];
         // dd($request_arr);
-        dump($request_arr);
+        // dump($request_arr);
         // dump($request->input('aa'));
         // return response()->view('welcome');
-        // return response()->json($input);
+        return response()->json($input);
+    }
+
+    // response
+    public function chkresponse(Response $response)
+    {
+        return response('Oops!', 500)
+            ->header('header_name', 'header_value')
+            ->cookie('cookie_name', 'cookie_value');
+
+        // return response()->json(['name' =>'Nicolas']);
+
+        // $dl_path = storage_path('app/public/docs/linux命令大全.pdf');
+        // return response()->download($dl_path);
     }
 
     // 縮網址
@@ -39,8 +54,9 @@ class UserController extends Controller
     }
 
     // open data
-    public function openDataUbike()
+    public function openDataUbike(Request $request)
     {
+        // dd($request->headers);
         $service = new OpenDataUbikeService();
         $ubkie_data = $service->getUbikeData();
         return response(['ubikeData' =>$ubkie_data]);
