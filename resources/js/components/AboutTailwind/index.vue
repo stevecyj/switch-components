16dc71f5faaa1d7269f814351a9a2a3ca5a0da28
steve_tsao <steve_tsao@neithnet.com>
2021-03-18 09:57:44 +0800

    add customize style, bootstrap like

--- resources/js/components/AboutTailwind/index.vue
+++ resources/js/components/AboutTailwind/index.vue
@@ -1,3 +1,10 @@
 <template>
-    <div class="text-superxl">hahaha</div>
+    <div>
+        <div class="text-superxl">hahaha</div>
+        <div class="bg-bootstrap_primary text-white">手榴蛋</div>
+        <div class="bg-bootstrap_secondary text-white">手榴蛋</div>
+        <div class="bg-bootstrap_success text-white">手榴蛋</div>
+        <div class="bg-bootstrap_danger text-white">手榴蛋</div>
+        <div class="bg-bootstrap_info text-white">手榴蛋</div>
+    </div>
 </template>
