458d627f1ed83137b71fa09d8550e7100f83850a
steve_tsao <steve_tsao@neithnet.com>
2021-03-22 12:27:28 +0800

    query builder tips, pluck

--- app/Http/Controllers/ResellerController.php
+++ app/Http/Controllers/ResellerController.php
@@ -12,7 +12,17 @@ class ResellerController extends Controller
         // $cert = DB::connection('mysql_test')->select('select * from cert');
         // $cert = DB::connection('mysql_test')->table('cert')->get();
         // $cert = DB::connection('mysql_test')->table('cert')->first();
-        $cert = DB::connection('mysql_test')->table('cert')->select('confirmed', 'visitor', 'hostname')->where('hostname', $hostname)->get();
+        // $cert = DB::connection('mysql_test')->table('cert')->select('confirmed', 'visitor', 'hostname')->where('hostname', $hostname)->get();
+
+        // 直接指定欄位
+        // $cert = DB::connection('mysql_test')->table('cert')->where('hostname', $hostname)->value('visitor');
+
+        // 是否有記錄
+        // $cert = DB::connection('mysql_test')->table('cert')->where('hostname', $hostname)->exists();
+
+        // 物件，以 id 為 key
+        $cert = DB::connection('mysql_test')->table('cert')->pluck('hostname', 'id');
+
         // dd($cert);
         // return response()->json($cert);
         return $cert;
